1b85e876b11a62b9e8884607b30c2ff9ff59f382
Martin Galvarini <13.martingalva@gmail.com>
2023-11-03 13:08:24 -0300

    Nuevo directorio

---
+++ aplicacion-bases-de-datos/go.mod
@@ -0,0 +1,5 @@
+module aplicacion-bases-de-datos
+
+go 1.19
+
+require github.com/lib/pq v1.10.9
--- aplicacion-bases-de-datos/main.go
+++ aplicacion-bases-de-datos/main.go
@@ -8,15 +8,6 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func dbConnection()(*sql.DB, error){
-       db, err := sql.Open("postgres", "user=postgres host=localhost dbname=Hospital sslmode=disable")
-       if err != nil{
-       
-       log.Fatal(err)
-}
-return db, nil
-}
-
 // crear base de datos
 
 func crearDB() {
@@ -27,12 +18,21 @@ func crearDB() {
 
 	defer db.Close()
 	_, err = db.Exec("create DATABASE Hospital")
-
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+// conexión a la base de datos
+ 
+func dbConnection()(*sql.DB, error){
+       db, err := sql.Open("postgres", "user=postgres host=localhost dbname=Hospital sslmode=disable")
+       if err != nil{
+       
+       log.Fatal(err)
+}
+	return db, nil
+}
 
 // crear tablas
 
@@ -40,19 +40,16 @@ func createTables() {
 	db, err := dbConnection()
 
 	_, err = db.Exec("DROP SCHEMA public CASCADE")
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	_, err = db.Exec("create SCHEMA publc")
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	_, err = db.Exec("create table paciente (nro_paciente int, nombre text, apellido text, dni paciente int, f_nac date, nro_obra_social int, nro afiliade int, domicilio text, telefono char(12), email text)")
-
 	if err != nil{
 		log.Fatal(err)
 	}
@@ -62,15 +59,16 @@ func createTables() {
 func main() {
 	_ = pq.QuoteIdentifier("some_text")
 	
-	dbConnection()
 	crearDB()
-	createTables()
 	
 	//conexion a base de datos postgres
 	db, err := sql.Open("postgres", "user=postgres host=localhost dbname=Hospital sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
+	
+	//dbConnection()
+	createTables()
+	
 	defer db.Close()
 }
