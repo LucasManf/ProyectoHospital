5a2a7e37316fec891e8f1592ce7e955a39a8a958
MartinGalva <13.martingalva@gmail.com>
2023-11-22 20:52:26 -0300

    funca json

--- hospital-json/app-boltdb.go
+++ hospital-json/app-boltdb.go
@@ -8,59 +8,58 @@ import (
     "strconv"
 )
 
-// SQL: nro_paciente
 // Go: NroPaciente
 type Paciente struct {
-	NroPaciente int // fixme
-	nombre  string
-	apellido string `json:apellido`
-	dni_paciente int
-	f_nacimiento string
-	nro_obra_social int
-	nro_afiliade int
-	domicilio string
-	telefono string
-	email string
+	NroPaciente int
+	Nombre  string
+	Apellido string
+	DniPaciente int
+	FechaNacimiento string
+	NroObraSocial int
+	NroAfiliade int
+	Domicilio string
+	Telefono string
+	Email string
 }
 
 type Medique struct {
-	dni_medique int
-	nombre string
-	apellido string
-	especialidad string
-	monto_consulta_privada float64
-	telefono string
+	DniMedique int
+	Nombre string
+	Apellido string
+	Especialidad string
+	MontoConsultaPrivada float64
+	Telefono string
 }
 
 type Consultorio struct {
-	nro_consultorio int
-	nombre string
-	domicilio string
-	codigo_postal int
-	telefono string
+	NroConsultorio int
+	Nombre string
+	Domicilio string
+	CodigoPostal int
+	Telefono string
 }
 
 type Turno struct {
-	nro_turno int
-	fecha string
-	nro_consultorio int
-	dni_medique int
-	nro_paciente int
-	nro_obra_social_consulta int
-	nro_afiliade_consulta int
-	monto_paciente float64
-	monto_obra_social float64
-	f_reserva string
-	estado string	
+	NroTurno int
+	Fecha string
+	NroConsultorio int
+	DniMedique int
+	NroPaciente int
+	NroObraSocialConsulta int
+	NroAfiliadeConsulta int
+	MontoPaciente float64
+	MontoObraSocial float64
+	FechaReserva string
+	Estado string	
 }
 
-type Obra_social struct {
-	nro_obra_social int
-	nombre string
-	contacto_nombre string
-	contacto_apellido string
-	contacto_telefono string
-	contacto_email string
+type ObraSocial struct {
+	NroObraSocial int
+	Nombre string
+	ContactoNombre string
+	ContactoApellido string
+	ContactoTelefono string
+	ContactoEmail string
 }
 
 func CreateUpdate(db *bolt.DB, bucketName string, key []byte, val []byte) error {
@@ -109,25 +108,25 @@ func main() {
 
 	//Ingreso datos
 	pacientes :=[]Paciente{
-		{nro_paciente: 1, nombre: "Martin", apellido: "Galvarini", dni_paciente: 42660991, f_nacimiento: "2000-06-30", nro_obra_social: 1, nro_afiliade: 1, domicilio: "Carlos Pellgrini 2436, Martinez", telefono: "11 4416-3214", email: "13.martingalva@gmail.com"},
-		{nro_paciente: 2, nombre: "Lucas", apellido: "Hombrefredi", dni_paciente: 43724987, f_nacimiento: "2000-11-05", nro_obra_social: 2, nro_afiliade: 1, domicilio: "Domingo de Acassuso 1121, La Lucila", telefono: "11 4491-3211", email: "luchetti@gmail.com"},
-		{nro_paciente: 3, nombre: "Veronica", apellido: "Suarez", dni_paciente: 40547321, f_nacimiento: "1998-05-03", nro_obra_social: 3, nro_afiliade: 1, domicilio: "Rivadavia 626, Belgrano", telefono: "11 4498-4321", email: "verosua@gmail.com"},
+		{NroPaciente: 1, Nombre: "Martin", Apellido: "Galvarini", DniPaciente: 42660991, FechaNacimiento: "2000-06-30", NroObraSocial: 1, NroAfiliade: 1, Domicilio: "Carlos Pellgrini 2436, Martinez", Telefono: "11 4416-3214", Email: "13.martingalva@gmail.com"},
+		{NroPaciente: 2, Nombre: "Lucas", Apellido: "Hombrefredi", DniPaciente: 43724987, FechaNacimiento: "2000-11-05", NroObraSocial: 2, NroAfiliade: 1, Domicilio: "Domingo de Acassuso 1121, La Lucila", Telefono: "11 4491-3211", Email: "luchetti@gmail.com"},
+		{NroPaciente: 3, Nombre: "Veronica", Apellido: "Suarez", DniPaciente: 40547321, FechaNacimiento: "1998-05-03", NroObraSocial: 3, NroAfiliade: 1, Domicilio: "Rivadavia 626, Belgrano", Telefono: "11 4498-4321", Email: "verosua@gmail.com"},
 	}
     
     mediques :=[]Medique{
-		{dni_medique: 398121041, nombre: "Carlos",	apellido: "Bilardo", especialidad: "Ginecologo", monto_consulta_privada: 1500.50, telefono: "11 4312-4574"},
+		{DniMedique: 398121041, Nombre: "Carlos", Apellido: "Bilardo", Especialidad: "Ginecologo", MontoConsultaPrivada: 1500.50, Telefono: "11 4312-4574"},
 	}
 	
 	consultorios :=[]Consultorio{
-		{nro_consultorio: 1, nombre: "Favaloro", domicilio: "Italia 231, Martinez", codigo_postal: 1640, telefono: "4799-4153"},
+		{NroConsultorio: 1, Nombre: "Favaloro", Domicilio: "Italia 231, Martinez", CodigoPostal: 1640, Telefono: "4799-4153"},
 	}
     
     turnos :=[]Turno{
-		{nro_turno: 1, fecha: "2023-11-23",	nro_consultorio: 1, dni_medique: 398121041,	nro_paciente: 1, nro_obra_social_consulta: 1, nro_afiliade_consulta: 1,	monto_paciente: 15000.00, monto_obra_social: 10000.00, f_reserva: "2023-09-14", estado: "reservado"},
+		{NroTurno: 1, Fecha: "2023-11-23",	NroConsultorio: 1, DniMedique: 398121041, NroPaciente: 1, NroObraSocialConsulta: 1, NroAfiliadeConsulta: 1,	MontoPaciente: 15000.00, MontoObraSocial: 10000.00, FechaReserva: "2023-09-14", Estado: "reservado"},
 	}
     
-    obras_sociales :=[]Obra_social{
-		{nro_obra_social: 1, nombre: "Galeno", contacto_nombre: "Gabriel", contacto_apellido: "Galindo",	contacto_telefono: "11 4414-44120",	contacto_email: "galeno@info.com"},
+    obras_sociales :=[]ObraSocial{
+		{NroObraSocial: 1, Nombre: "Galeno", ContactoNombre: "Gabriel", ContactoApellido: "Galindo", ContactoTelefono: "11 4414-44120", ContactoEmail: "galeno@info.com"},
 	}
     
     //Escritura de datos
@@ -137,7 +136,7 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		CreateUpdate(db, "pacientes", []byte(strconv.Itoa(paciente.nro_paciente)), data)
+		CreateUpdate(db, "pacientes", []byte(strconv.Itoa(paciente.NroPaciente)), data)
 	} 
 	
 	for _, medique:= range mediques {
@@ -145,7 +144,7 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		CreateUpdate(db, "mediques", []byte(strconv.Itoa(medique.dni_medique)), data)
+		CreateUpdate(db, "mediques", []byte(strconv.Itoa(medique.DniMedique)), data)
 	} 
 	
 	for _, consultorio:= range consultorios {
@@ -153,7 +152,7 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		CreateUpdate(db, "consultorios", []byte(strconv.Itoa(consultorio.nro_consultorio)), data)
+		CreateUpdate(db, "consultorios", []byte(strconv.Itoa(consultorio.NroConsultorio)), data)
 	} 
 	
 	for _, turno:= range turnos {
@@ -161,7 +160,7 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		CreateUpdate(db, "turnos", []byte(strconv.Itoa(turno.nro_turno)), data)
+		CreateUpdate(db, "turnos", []byte(strconv.Itoa(turno.NroTurno)), data)
 	} 
 	
 	for _, obra_social:= range obras_sociales {
@@ -169,12 +168,12 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		CreateUpdate(db, "obras sociales", []byte(strconv.Itoa(obra_social.nro_obra_social)), data)
+		CreateUpdate(db, "obras sociales", []byte(strconv.Itoa(obra_social.NroObraSocial)), data)
 	}
 	
 	//Lectura de datos
 	for _, paciente:= range pacientes {
-		resultado, err := ReadUnique(db, "pacientes", []byte(strconv.Itoa(paciente.nro_paciente)))
+		resultado, err := ReadUnique(db, "pacientes", []byte(strconv.Itoa(paciente.NroPaciente)))
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -182,7 +181,7 @@ func main() {
 	} 
 	
 	for _, medique:= range mediques {
-		resultado, err := ReadUnique(db, "mediques", []byte(strconv.Itoa(medique.dni_medique)))
+		resultado, err := ReadUnique(db, "mediques", []byte(strconv.Itoa(medique.DniMedique)))
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -190,7 +189,7 @@ func main() {
 	} 
 	
 	for _, consultorio:= range consultorios {
-		resultado, err := ReadUnique(db, "consultorios", []byte(strconv.Itoa(consultorio.nro_consultorio)))
+		resultado, err := ReadUnique(db, "consultorios", []byte(strconv.Itoa(consultorio.NroConsultorio)))
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -198,7 +197,7 @@ func main() {
 	} 
 	
 	for _, turno:= range turnos {
-		resultado, err := ReadUnique(db, "turnos", []byte(strconv.Itoa(turno.nro_turno)))
+		resultado, err := ReadUnique(db, "turnos", []byte(strconv.Itoa(turno.NroTurno)))
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -206,7 +205,7 @@ func main() {
 	} 
 	
 	for _, obra_social:= range obras_sociales {
-		resultado, err := ReadUnique(db, "obras sociales", []byte(strconv.Itoa(obra_social.nro_obra_social)))
+		resultado, err := ReadUnique(db, "obras sociales", []byte(strconv.Itoa(obra_social.NroObraSocial)))
 		if err != nil {
 			log.Fatal(err)
 		}
